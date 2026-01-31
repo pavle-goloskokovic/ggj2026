5a253913175dbb91d81618de385bfda246eaeadf
Pavle <pgoloskokovic@gmail.com>
2026-01-31 21:51:19 +0100

    accessories items added to game

--- src/ts/scenes/Game.ts
+++ src/ts/scenes/Game.ts
@@ -2,6 +2,7 @@ import Phaser from 'phaser';
 import Scene = Phaser.Scene;
 import AvatarBase from '../classes/AvatarBase';
 import SelectableItem from '../classes/SelectableItem';
+import { getRandomSpriteName } from '../constants';
 
 /**
  * Game Phaser scene.
@@ -47,5 +48,27 @@ export class Game extends Scene {
         }
 
         bases[0].setSelected(true);
+
+        const itemCategories = ['accessory', 'facial', 'glasses', 'hair', 'hat'] as const;
+        const itemCount = 10;
+        const itemsPerRow = 5;
+        const row1Y = scale.height * 0.3;
+        const row2Y = scale.height * 0.5;
+        const itemSpacing = scale.width / (itemsPerRow + 1);
+        const items: SelectableItem[] = [];
+
+        for (let i = 0; i < itemCount; i++)
+        {
+            const rowIndex = i < itemsPerRow ? 0 : 1;
+            const colIndex = i % itemsPerRow;
+            const x = itemSpacing * (colIndex + 1);
+            const y = rowIndex === 0 ? row1Y : row2Y;
+            const category = itemCategories[i % itemCategories.length];
+            const spriteName = getRandomSpriteName(category);
+            const image = this.add.image(0, 0, 'sprites', spriteName);
+            const item = new SelectableItem(this, x, y, image)
+                .setScale(3);
+            items.push(item);
+        }
     }
 }
